2c469cce4ef87d88a9352702059ddad1dbef612d
Alexia Skrz <alexia.sireuille@gmail.com>
2022-08-23 01:55:15 +0200

    integration header mobile first

---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+import "./Banner.css"
+// import logo from "../assets/logo.png"
+
+function Banner ()
+{
+  
+  return (
+    <h1 className="header__logo"> </h1>
+  )
+}
+
+export default Banner
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import "./Header.css"
+import Banner from "./Banner"
+import Nav from "./Nav"
+
+function Header ()
+{
+  return (
+    <header className="header">
+      <Banner />
+      <Nav />
+    </header>)
+}
+
+export default Header
