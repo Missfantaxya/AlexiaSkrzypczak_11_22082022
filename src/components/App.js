14af302f4ee65fb778814448d8f996f48d8eb37a
Alexia Skrz <alexia.sireuille@gmail.com>
2022-08-23 21:06:40 +0200

    mise en plce de la route provisoire vers la page lodging

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,22 @@
-import './App.css';
+import { Routes, Route} from 'react-router-dom';
+
+import './App.css'
+
 import Header from './Header';
 import Home from './Home';
+import Lodging from './Lodging'
 import Footer from './Footer';
-
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <main className='main'>
-        <Home />
-      </main>
+        <main className='main'>
+          <Routes>
+            <Route path="/" element={ <Home /> } />
+            <Route path="lodging" element={<Lodging />} />
+          </Routes>
+        </main>
       <Footer />
     </div>
   );
---
+++ src/components/Lodging.js
@@ -0,0 +1,13 @@
+import "./Lodging.css"
+
+function Lodging()
+{
+  return (
+    <>
+      <p>composant 1</p>
+      <p>composant 2</p>
+    </>
+  )
+}
+
+export default Lodging
