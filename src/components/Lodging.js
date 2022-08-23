a1f18c386dee6b867835f578b02fdf2ce49dec34
Alexia Skrz <alexia.sireuille@gmail.com>
2022-08-23 23:16:24 +0200

    lodging title style & layout WIP

--- src/components/Lodging.js
+++ src/components/Lodging.js
@@ -7,7 +7,24 @@ function Lodging()
   return (
     <>
       <Carrousel />
-      <p>composant 2</p>
+      <div className="lodging__content">
+        <div className="lodging__title">
+            <h2 className="lodging__description">Paris center, on the romantic Canal Saint-Martin</h2>
+            <p className="lodging__location">Paris, Île-de-France</p>
+        </div>
+        <div className="lodging__details">
+          <div> Les tags FAIRE COMPOSANT</div>
+          <div> Notation FAIRE COMPOSANT </div>
+        </div>
+        <div className="lodging__host">
+          <div className="host__name">
+            <p>Alexandre</p>
+            <p>Dumas</p>
+          </div>
+          <div className="host__picture">photo</div>
+        </div>
+        
+      </div>
     </>
   )
 }
