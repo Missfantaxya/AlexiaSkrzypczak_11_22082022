064f0f7c157670074cea9d9fcaedd510652efd51
Alexia Skrz <alexia.sireuille@gmail.com>
2022-08-23 14:42:11 +0200

    integration mask desktop

--- src/components/Mask.js
+++ src/components/Mask.js
@@ -5,7 +5,7 @@ function Mask ()
   return (
     <section className="home__mask">
         <div className="mask__background">
-          <p className="mask__content">Chez vous,
+          <p className="mask__content">Chez vous,&nbsp;
             <span>partout et ailleurs</span>
           </p>
         </div>
