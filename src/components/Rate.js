1245ba269d013115f7c29595921dd53c4f63d76f
Alexia Skrz <alexia.sireuille@gmail.com>
2022-09-28 13:25:19 +0200

    HostDetails mobile first

--- src/components/Rate.js
+++ src/components/Rate.js
@@ -2,10 +2,12 @@ import './Rate.css'
 
 
 const Star = (size, color) => (
-  <svg width={ size } height={ size } fill={ color } xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M9.8225 6.5L8 0.5L6.1775 6.5H0.5L5.135 9.8075L3.3725 15.5L8 11.9825L12.635 15.5L10.8725 9.8075L15.5 6.5H9.8225Z" />
-  </svg>
+  <div className='host__star'>
+    <svg width={ size } height={ size } fill={ color } xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M9.8225 6.5L8 0.5L6.1775 6.5H0.5L5.135 9.8075L3.3725 15.5L8 11.9825L12.635 15.5L10.8725 9.8075L15.5 6.5H9.8225Z" />
+    </svg>
+  </div>
 )
 
 function Rate() {
