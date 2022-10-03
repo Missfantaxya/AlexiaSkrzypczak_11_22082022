932d84f5eb4b5c4c41d3b741adff1caf1b3528c5
Alexia Skrz <alexia.sireuille@gmail.com>
2022-10-03 14:48:27 +0200

    WIP responsive strar's rate

--- src/components/Rate.js
+++ src/components/Rate.js
@@ -1,22 +1,39 @@
 import './Rate.css'
 
-// TODO modifier la taille du svg selon la taille de l'écran
-const Star = (size, color) => (
+
+
+function Rate ()
+{
+  let StarSize
+  if ( window.screen.width <= 375 )
+  {
+  StarSize= 15
+  }
+  else if ( window.screen.width > 375 && window.screen.width < 1440 )
+  {
+    // TODO voir pour le choix d ela valeur
+    StarSize= 25
+  }
+  else if ( window.screen.width >= 1440 ){
+    StarSize= 30
+  }
+
+   //FIXME erreur console sur la width du svg
+const Star = (color) => (
   <div className='host__star'>
-    <svg width={ size } height={ size } fill={ color } xmlns="http://www.w3.org/2000/svg">
-      <path d="M9.8225 6.5L8 0.5L6.1775 6.5H0.5L5.135 9.8075L3.3725 15.5L8 11.9825L12.635 15.5L10.8725 9.8075L15.5 6.5H9.8225Z" />
+    <svg width={ StarSize } height={ StarSize } fill={ color } xmlns="http://www.w3.org/2000/svg">
+      <path d="M9.8225 6.5L8 0.5L6.1775 6.5H0.5L5.135 9.8075L3.3725 15.5L8 11.9825L12.635 15.5L10.8725 9.8075L15.5 6.5H9.8225Z" width={ StarSize } height={ StarSize } fill={ color } />
     </svg>
   </div>
-)
+  )
 
-function Rate() {
     return(
       <div className='host__rate'>
-        { Star( 15, "#FF6060" ) }
-        { Star( 15, "#FF6060" ) }
-        { Star( 15, "#FF6060" ) }
-        { Star( 15, "#E3E3E3" ) }
-        { Star( 15, "#E3E3E3" ) }
+        { Star( "#FF6060" ) }
+        { Star( "#FF6060" ) }
+        { Star("#FF6060" ) }
+        { Star( "#E3E3E3" ) }
+        { Star( "#E3E3E3" ) }
       </div>
     )
 }
