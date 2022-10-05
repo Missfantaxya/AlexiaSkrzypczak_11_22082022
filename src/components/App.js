e6c5f4ba5f74ed66cf4990b1d8f36c7f5cf11ec2
Alexia Skrz <alexia.sireuille@gmail.com>
2022-10-05 14:27:30 +0200

    fix

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,10 @@ import { Routes, Route} from 'react-router-dom';
 
 import './App.css'
 
-import Header from './Header';
-import Home from './Home';
+import Header from './Header'
+import Home from './Home'
 import Lodging from './Lodging'
-import Footer from './Footer';
+import Footer from './Footer'
 
 function App() {
   return (
