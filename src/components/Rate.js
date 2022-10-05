ba89656c6a4562b8a8da2004e612dcbce05c5307
Alexia Skrz <alexia.sireuille@gmail.com>
2022-10-05 10:35:00 +0200

    Star style mobile & desktop

--- src/components/Rate.js
+++ src/components/Rate.js
@@ -4,30 +4,23 @@ import './Rate.css'
 
 function Rate ()
 {
-  let StarSize
-  if ( window.screen.width <= 375 )
-  {
-  StarSize= 15
-  }
-  else if ( window.screen.width > 375 && window.screen.width < 1440 )
-  {
-    // TODO voir pour le choix d ela valeur
-    StarSize= 25
-  }
-  else if ( window.screen.width >= 1440 ){
-    StarSize= 30
-  }
-
-   //FIXME erreur console sur la width du svg
 const Star = (color) => (
   <div className='host__star'>
-    <svg width={ StarSize } height={ StarSize } fill={ color } xmlns="http://www.w3.org/2000/svg">
-      <path d="M9.8225 6.5L8 0.5L6.1775 6.5H0.5L5.135 9.8075L3.3725 15.5L8 11.9825L12.635 15.5L10.8725 9.8075L15.5 6.5H9.8225Z" width={ StarSize } height={ StarSize } fill={ color } />
+    <svg
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg" >
+      <path
+        d="M9.8225 6.5L8 0.5L6.1775 6.5H0.5L5.135 9.8075L3.3725 15.5L8 11.9825L12.635 15.5L10.8725 9.8075L15.5 6.5H9.8225Z"
+        fill={ color }
+      />
     </svg>
   </div>
   )
 
-    return(
+
+
+    return (
       <div className='host__rate'>
         { Star( "#FF6060" ) }
         { Star( "#FF6060" ) }
