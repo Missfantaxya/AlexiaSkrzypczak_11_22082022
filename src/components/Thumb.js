bc0449fcd9fee257ad4a9c7c4d43df99e9252461
Alexia Skrz <alexia.sireuille@gmail.com>
2022-10-11 09:21:44 +0200

    dynamic cover to homepage

--- src/components/Thumb.js
+++ src/components/Thumb.js
@@ -4,28 +4,21 @@ import "./Thumb.css"
 
 // TODO rendre le lien dynamique (https://reactrouter.com/en/main/route/route#dynamic-segments)
 
-// FIXME créer une var avec {cover} pour l'utiliser en background image à la place de la balise image.
-
 function Thumb ( {
   id,
   title,
   cover,
 } )
-{
-  let coverImage = {cover}
-  let divStyle = {
-    backgroundImage: "url("+{ coverImage }+")",
-    background: 'linear-gradient(180deg,rgba(255, 255, 255, 0) 0%,rgba(0, 0, 0, 0.5) 100%)'
-}
-  return (
+  {
+return (
     <article className="thumb">
       <Link to="/lodging"  >
-        <div className="thumb__background" style={divStyle} >
-          {/* <img
+        <div className="thumb__background" >
+          <img
             className="thumb__cover"
             src={ cover }
             alt=""
-          /> */}
+          />
           <h2 className="thumb__content">
             { title }
           </h2>
