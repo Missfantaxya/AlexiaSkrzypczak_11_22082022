ef7b29f2548bd1fad9a4593a2c567e953b981826
Alexia Skrz <alexia.sireuille@gmail.com>
2022-10-19 12:03:07 +0200

    carrousel d'images

--- src/components/Carrousel.js
+++ src/components/Carrousel.js
@@ -1,34 +1,68 @@
-import './Carrousel.css'
+import { useState } from 'react'
 
-// passer par le css
+import './Carrousel.css'
 
 function Carrousel ({pictures})
 {
-  // console.log("pictures : ",pictures) //* array
+  console.log( "pictures : ", pictures ) //* array
+  // console.log("pictures.lenght -1 : ",pictures.length - 1)//*
+  
+  const [ pictureIndex, setPictureIndex ] = useState( 0 )
+  
+  function HandlePreviousClick ()
+  {
+    if ( pictureIndex === 0 )
+    {
+      let lastPicture = pictures.length-1
+      setPictureIndex( lastPicture )
+    } else
+    {
+      let previousPicture = pictureIndex - 1
+      setPictureIndex( previousPicture )
+    }
+  }
+  
+  function HandleNextClick ()
+  {
+    if ( pictureIndex === pictures.length-1 )
+    {
+      let firstPicture = 0
+      setPictureIndex( firstPicture )
+    } else
+    {
+      let nextPicture = pictureIndex + 1
+      setPictureIndex( nextPicture )
+    }
+  }
+
+
+
   
   const multipesPictures = pictures.length > 1
-  function handlClickPrevious ()
-  {
-  // https://www.youtube.com/watch?v=fjsJLcGWjcU
-  }
 
   return ( 
     <div className="carrousel">
-        {multipesPictures && <button className='carrousel__button carrousel__previous'>
-      précédent
-    </button>}
-      <div className='carroussel__container'>
-        {pictures.map((picture, index) =>
+      { multipesPictures &&
+        <button
+          className='carrousel__button carrousel__previous'
+          onClick={ HandlePreviousClick }
+        >
+          précédent
+        </button>
+      }
           <img
-            key={index}
-          src={ picture }
+          src={ pictures[pictureIndex] }
           alt="logement"
-          className = "carrousel__picture" />
-        ) }
-      </div>
-      {multipesPictures &&  <button className='carrousel__button carrousel__next'>
-      suivant
-    </button>}
+        className="carrousel__picture"
+      />
+      { multipesPictures &&
+        <button
+          className='carrousel__button carrousel__next'
+          onClick={HandleNextClick}
+        >
+          suivant
+        </button>
+      }
     </div>
   )
 }
