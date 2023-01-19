a8f2d200608825d4556630399a2f1e2e47350caf
Alexia Skrz <alexia.sireuille@gmail.com>
2023-01-19 14:05:53 +0100

    Fix scrollToTop

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
+import ScrollToTop from './utils/ScrollToTop';
+
 import './index.css';
 
 import App from './components/App';
@@ -10,6 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop/>
       <App />
     </BrowserRouter>
   </React.StrictMode>
